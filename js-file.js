eb67f5e052de2a1bc4ef85dfb1eb7f02e329fedb
Rahul-R-24 <rahulram2204@gmail.com>
2024-01-12 05:02:45 -0500

    1

---
+++ js-file.js
@@ -0,0 +1,19 @@
+function alertFunction() {
+    alert("YAY! YO");
+}
+const btn = document.querySelector('#btn')
+  
+// METHOD 3
+btn.addEventListener('click', alertFunction);
+
+btn.addEventListener('click', function (e) {
+  console.log(e);
+});
+
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+});
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
